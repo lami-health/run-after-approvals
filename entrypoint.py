4ec6e028259d3c3e22b2fc69e99d8dfe9e4024b4
CherryRamatis <cherry.ramatis@lamimed.it>
2022-04-14 20:29:41 +0000

    feat: add env vars and fix dockerfile

--- entrypoint.py
+++ entrypoint.py
@@ -4,9 +4,27 @@ import os
 import json
 import requests
 
-api_url = "https://api.github.com/repos/cherryramatisdev/website-ci/pulls/16/reviews?per_page=100"
+approvals = os.getenv("APPROVALS") or 2
+github_token = os.getenv("GITHUB_TOKEN")
+github_repository = os.getenv("GITHUB_REPOSITORY")
+github_event_path = os.getenv("GITHUB_EVENT_PATH")
+
+if not github_token:
+    print("Set the GITHUB_TOKEN env variable")
+    exit(1)
+
+if not github_repository:
+    print("Set the GITHUB_REPOSITORY env variable")
+    exit(1)
+
+if not github_event_path:
+    print("Set the GITHUB_EVENT_PATH env variable")
+    exit(1)
+
+pull_request_number = github_event_path["pull_request"]["number"]
+api_url = f"https://api.github.com/repos/{github_repository}/pulls/{pull_request_number}/reviews?per_page=100"
 headers = {
-    "Authorization": "token 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",
+    "Authorization": f"token {github_token}",
     "Accept": "application/vnd.github.v3+json",
 }
 
@@ -21,10 +39,10 @@ def make_request():
 
         acc = acc + 1
 
-    label = 'APPROVED' if acc >= 2 else 'DENIED'
-    print(f"{acc}/2 Approvals - {label}")
+    label = "APPROVED" if acc >= approvals else "DENIED"
+    print(f"{acc}/{approvals} Approvals - {label}")
 
-    if acc < 2:
+    if acc < approvals:
         exit(1)
     return
 
